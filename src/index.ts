12a65909c39aa6fb35fa7176d86fe6f7afa39dfa
novando <rizkynovando@gmail.com>
2026-02-10 09:20:48 +0700

    feature: dockerfile

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ async function runMigrations() {
   await loadVaultConfig()
 
   const action = getArg("action", "up") ?? "up";
-  const driver = getArg("driver", "postgres") ?? "postgres";
+  const driver = getArg("driver", "postgres") ?? "";
   const type = getArg("type", "migration") ?? "migration";
   const name = getArg("name", "") ?? "";
 
@@ -23,7 +23,11 @@ async function runMigrations() {
     case "clickhouse":
       await ch(type, action, name);
       break;
+    case "postgres":
+      await pg(type, action, name);
+      break;
     default:
+      await ch(type, action, name);
       await pg(type, action, name);
       break;
   }
